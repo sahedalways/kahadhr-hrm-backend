5d1c2dceb78cf608531bdd9fb83a6c7d24daa416
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2025-11-20 15:03:34 +0600

    added route

--- routes/dev-tools.php
+++ routes/dev-tools.php
@@ -41,3 +41,23 @@ Route::get('/send-test-mail', function () {
 
   return 'Test email sent to ssahed65@gmail.com';
 });
+
+
+
+Route::get('/composer-update', function () {
+  if (app()->environment() !== 'local') {
+    return response()->json(['message' => 'Not allowed in production'], 403);
+  }
+
+  $output = [];
+  $return_var = 0;
+
+  // Run composer update
+  exec('composer update 2>&1', $output, $return_var);
+
+  return response()->json([
+    'message' => 'Composer update completed!',
+    'output' => $output,
+    'return_code' => $return_var,
+  ]);
+});

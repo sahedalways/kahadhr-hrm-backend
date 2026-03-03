87ef2f2c39c6a9c383b175d734c60675f5ccea9e
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-03-03 12:08:32 +0600

    done

--- app/Livewire/Backend/Employee/Auth/EmployeeLogin.php
+++ app/Livewire/Backend/Employee/Auth/EmployeeLogin.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Hash;
 
 class EmployeeLogin extends BaseComponent
 {
-    public $email = "emp@xyz.com";
-    public $password = "12345678";
+    public $email = "";
+    public $password = "";
     public $company;
     public $rememberMe = false;
 

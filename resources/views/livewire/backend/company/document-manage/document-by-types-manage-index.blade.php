c9011bcfd4afbc8d028884380b2ecb4583f7f04b
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-04-07 10:24:08 +0600

    done

--- resources/views/livewire/backend/company/document-manage/document-by-types-manage-index.blade.php
+++ resources/views/livewire/backend/company/document-manage/document-by-types-manage-index.blade.php
@@ -576,7 +576,7 @@
                             <div class="mt-2 small fst-italic text-secondary">
                                 <i class="bi bi-cloud-upload me-1 text-primary"></i> 3 MB max •
                                 <i class="bi bi-file-earmark-plus me-1 text-success"></i> 1 file at a time •
-                                <i class="bi bi-filetype-pdf me-1 text-purple"></i> PDF, JPG, PNG, GIF, WebP
+                                <i class="bi bi-filetype-pdf me-1 text-purple"></i> PDF, JPG, JPEG, PNG, GIF, WebP
                             </div>
 
 
